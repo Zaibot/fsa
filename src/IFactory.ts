97a279774fcf3aa46382679fc7038958dd2acc52
Tobias de Groen <tdgroen@zaibot.net>
2018-02-07 08:13:51 +0100

    feature(factory): add interface properties for referencing action/payload/meta type
    workaround, solvable by https://github.com/Microsoft/TypeScript/issues/6606

--- src/IFactory.ts
+++ src/IFactory.ts
@@ -2,6 +2,12 @@ import { IAction } from './IAction';
 
 export interface IFactory<TPayload = any, TMeta = any> {
     readonly type: symbol | string;
+    /* workaround for getting the payload type, value not defined */
+    readonly Payload: TPayload;
+    /* workaround for getting the meta type, value not defined */
+    readonly Meta: TMeta;
+    /* workaround for getting the action type, value not defined */
+    readonly Action: IAction<TPayload, TMeta>;
 
     (payload: TPayload, meta?: TMeta): IAction<TPayload, TMeta>;
     error(error: Error, meta?: TMeta): IAction<Error, TMeta>;
