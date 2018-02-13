780bc4705b0b2eff16d7e048999d63f08b87b845
Tobias de Groen <tdgroen@zaibot.net>
2018-02-13 14:49:19 +0100

    fix(isTypeOneOf): type union

--- src/isTypeOneOf.ts
+++ src/isTypeOneOf.ts
@@ -2,15 +2,27 @@ import { IAction } from './IAction';
 import { IFactory } from './IFactory';
 import { isAction } from './isAction';
 
-export function isTypeOneOf
-    <TPayload1, TPayload2>(action: any, ...factories: IFactory[]):
-    action is IAction<TPayload1, any> | IAction<TPayload2, any>;
-export function isTypeOneOf
-    <TPayload1, TPayload2, TPayload3>(action: any, ...factories: IFactory[]):
-    action is IAction<TPayload1, any> | IAction<TPayload2, any> | IAction<TPayload3, any>;
-export function isTypeOneOf
-    <TPayload1, TPayload2, TPayload3, TPayload4>(action: any, ...factories: IFactory[]):
-    action is IAction<TPayload1, any> | IAction<TPayload2, any> | IAction<TPayload3, any> | IAction<TPayload4, any>;
+export function isTypeOneOf<TPayload1, TPayload2, TMeta1, TMeta2>(
+    action: any,
+    factory1: IFactory<TPayload1, TMeta1>,
+    factory2: IFactory<TPayload2, TMeta2>):
+    action is IAction<TPayload1, TMeta1> | IAction<TPayload2, TMeta2>;
+
+export function isTypeOneOf<TPayload1, TPayload2, TPayload3, TMeta1, TMeta2, TMeta3>(
+    action: any,
+    factory1: IFactory<TPayload1, TMeta1>,
+    factory2: IFactory<TPayload2, TMeta2>,
+    factory3: IFactory<TPayload3, TMeta3>):
+    action is IAction<TPayload1, TMeta1> | IAction<TPayload2, TMeta2> | IAction<TPayload3, TMeta3>;
+
+export function isTypeOneOf<TPayload1, TPayload2, TPayload3, TPayload4, TMeta1, TMeta2, TMeta3, TMeta4>(
+    action: any,
+    factory1: IFactory<TPayload1, TMeta1>,
+    factory2: IFactory<TPayload2, TMeta2>,
+    factory3: IFactory<TPayload3, TMeta3>,
+    factory4: IFactory<TPayload4, TMeta4>):
+    action is IAction<TPayload1, TMeta1> | IAction<TPayload2, TMeta2> | IAction<TPayload3, TMeta3> | IAction<TPayload4, TMeta4>;
+
 export function isTypeOneOf(action: any, ...factories: IFactory[]): action is IAction {
     return isAction(action) ? factories.some((factory) => action.type === factory.type) : false;
 }
